ad12d53aa350615006ad879568de5545478edb56
Nazmul <nazmulislamn95@gmail.com>
2022-05-09 19:29:45 +0600

    JavaScript Set

---
+++ style.js
@@ -0,0 +1,26 @@
+//every words in different area
+// const mySet = new Set();
+
+// mySet.add('a');
+// mySet.add('b');
+// mySet.add('c');
+// mySet.add('d');
+
+// console.log(mySet);
+
+
+
+
+// const myNewSet = new Set(['a', 'b', 'c']);
+
+// let sum = ' ';
+// myNewSet.forEach(function(value){
+//     sum = sum + value;
+// })
+// console.log(sum);
+
+
+
+
+const myNewSet = new Set(['a', 'b', 'c']);
+console.log(myNewSet.values());
